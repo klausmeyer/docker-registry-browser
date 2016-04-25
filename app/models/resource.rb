f24593e32a8a58130722344fb5c0c26796bca642
Klaus Meyer <spam@klaus-meyer.net>
2016-04-25 20:38:10 +0200

    add new options to allow basic-auth against registry (closing #1)

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -6,6 +6,11 @@ class Resource
       options = { url: Rails.configuration.x.registry_url }
       options.merge!(ssl: { verify: false }) if Rails.configuration.x.no_ssl_verification
       Faraday.new(options) do |f|
+        if Rails.configuration.x.basic_auth_user && Rails.configuration.x.basic_auth_password
+          f.use Faraday::Request::BasicAuthentication,
+            Rails.configuration.x.basic_auth_user,
+            Rails.configuration.x.basic_auth_password
+        end
         f.request  :url_encoded
         f.response :logger unless Rails.env.test?
         f.response :raise_error
--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,8 @@ module DockerRegistryBrowser
     config.x.version             = "v0.0.1-dev"
     config.x.registry_url        = ENV["DOCKER_REGISTRY_URL"] || "http://localhost:5000"
     config.x.no_ssl_verification = ENV["NO_SSL_VERIFICATION"].in? %w(1 true yes)
+    config.x.basic_auth_user     = ENV["BASIC_AUTH_USER"]
+    config.x.basic_auth_password = ENV["BASIC_AUTH_PASSWORD"]
     config.x.delete_enabled      = ENV["ENABLE_DELETE_IMAGES"].in? %w(1 true yes)
   end
 end
