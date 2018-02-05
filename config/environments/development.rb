778dfc3905b58688f0ef7f71724c4b1a91ef12c9
Klaus Meyer <spam@klaus-meyer.net>
2018-02-05 15:39:12 +0100

    fix rails development config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   config.active_support.deprecation = :log
 
   # Highlight code that triggered database queries in logs.
-  config.active_record.verbose_query_logs = true
+  # config.active_record.verbose_query_logs = true
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
