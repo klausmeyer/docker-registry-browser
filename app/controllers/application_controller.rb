13c282c4483c11d551e474fd32f69daa467c1e1e
Klaus Meyer <spam@klaus-meyer.net>
2018-11-21 12:24:37 +0100

    pass http basic auth from registry to browser

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,25 @@
 class ApplicationController < ActionController::Base
-  rescue_from Faraday::ResourceNotFound, with: :render_404
+  rescue_from Faraday::ResourceNotFound, with: :not_found
+  rescue_from Faraday::ClientError, with: :client_error
+
+  before_action do
+    Current.http_basic_auth = ActionController::HttpAuthentication::Basic::user_name_and_password(request)
+  end
 
   private
 
-  def render_404
+  def not_found
     render file: "#{Rails.root}/public/404.html", layout: false, status: 404
+  end
+
+  def client_error(error)
+    if error.response &&
+      error.response[:status] == 401 &&
+      error.response.dig(:headers, 'www-authenticate') =~ /basic/i
+
+      request_http_basic_authentication
+    else
+      raise
+    end
   end
 end
