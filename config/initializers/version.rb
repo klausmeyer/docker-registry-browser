a49b1208c46856497ccf9e0df717760743df7c74
Klaus Meyer <spam@klaus-meyer.net>
2018-12-10 18:04:45 +0100

    release v1.0.0

---
+++ config/initializers/version.rb
@@ -0,0 +1,7 @@
+Rails.application.config.x.version = "1.0.0"
+
+# Docker Cloud will pass the git commit it is building the image from
+# See: https://docs.docker.com/docker-hub/builds/advanced/
+if sha = ENV["SOURCE_COMMIT"].presence
+  Rails.application.config.x.version << " (#{sha[0...7]})"
+end
