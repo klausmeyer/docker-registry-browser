53fcea65e742f5a3160f85436273a98962d01d30
Johan Smits <johan.smits@leftclick.eu>
2019-04-25 19:55:30 +0200

    Add direct routes for easy copy paste of the repo url (#177)

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   root to: 'repositories#index'
 
-  get 'repo/*repo/tag/*tag', to: 'tags#show',         as: :tag       , constraints: { repo: /.+/, tag: /[^\/]+/ }
-  get 'repo/*repo',          to: 'repositories#show', as: :repository, constraints: { repo: /.+/ }
+  get 'repo/*repo/tag/*tag',    to: 'tags#show',         as: :tag,         constraints: {repo: /.+/, tag: /[^\/]+/}
+  get 'repo/*repo',             to: 'repositories#show', as: :repository,  constraints: {repo: /.+/}
 
-  delete 'repo/*repo/tag/*tag', to: 'tags#destroy', constraints: { repo: /.+/, tag: /[^\/]+/ }
+  get '*repo:*tag',             to: redirect('repo/%{repo}/tag/%{tag}'),   constraints: {repo: /.+/, tag: /[^\/]+/}
+  get '*repo',                  to: redirect('repo/%{repo}'),              constraints: {repo: /.+/}
 
-  get :ping, to: Proc.new { [200, {'Content-Type' => 'text/plain'}, ['pong']] }
+  delete 'repo/*repo/tag/*tag', to: 'tags#destroy',                              constraints: {repo: /.+/, tag: /[^\/]+/}
+
+  get :ping, to: Proc.new {[200, {'Content-Type' => 'text/plain'}, ['pong']]}
 end
