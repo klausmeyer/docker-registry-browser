f962e0019b26a355242949cf1daaa7eee65cb158
Craig Minihan <craig@ripcordsoftware.com>
2019-11-29 06:00:11 +0000

    Allow Faraday to follow remote redirects (#246)

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -12,6 +12,7 @@ class Resource
       elsif (token = Current.http_token_auth).present?
         f.request :oauth2, token, token_type: :bearer
       end
+      f.use FaradayMiddleware::FollowRedirects, limit: 5
       f.use FaradayMiddleware::ParseJson, content_type: /json|prettyjws/
       f.response :logger unless Rails.env.test?
       f.response :raise_error
