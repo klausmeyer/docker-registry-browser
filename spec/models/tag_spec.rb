6f5648c27d898f3410fdca92b40bd4b04bc0f249
Klaus Meyer <spam@klaus-meyer.net>
2019-12-03 19:20:56 +0100

    support registries returning json header for blob requests (#248)

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -5,11 +5,26 @@ describe Tag do
 
   describe ".find" do
     let(:name) { "latest" }
-    
-    it "returns one repository" do
-      VCR.use_cassette("tag/find") do
-        tag = Tag.find repository: repo, name: name
-        expect(tag).to be_instance_of Tag
+
+    context "with a repository returning non-json content type for meta blob" do
+      let(:blob_content_type) { "application/octet-stream" }
+
+      it "returns one repository" do
+        VCR.use_cassette("tag/find", erb: {blob_content_type: blob_content_type}) do
+          tag = Tag.find repository: repo, name: name
+          expect(tag).to be_instance_of Tag
+        end
+      end
+    end
+
+    context "with a repository returning json content type for meta blob" do
+      let(:blob_content_type) { "application/vnd.docker.container.image.v1+json" }
+
+      it "returns one repository" do
+        VCR.use_cassette("tag/find", erb: {blob_content_type: blob_content_type}) do
+          tag = Tag.find repository: repo, name: name
+          expect(tag).to be_instance_of Tag
+        end
       end
     end
   end
