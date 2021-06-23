3306cfaf3aa9e1400e49bb5d9e4438fe7ed3e292
Klaus Meyer <spam@klaus-meyer.net>
2021-06-23 22:02:15 +0200

    add support for oci image format (#410)

--- app/models/history_entry.rb
+++ app/models/history_entry.rb
@@ -1,14 +1,15 @@
 class HistoryEntry
   include ActiveModel::Model
 
-  attr_accessor :created, :created_by
+  attr_accessor :created, :comment, :created_by
 
   def initialize(attrs = {})
     self.created    = Time.parse(attrs['created']) rescue nil
+    self.comment    = attrs['comment']
     self.created_by = attrs['created_by']
   end
 
   def to_s
-    "[#{created}] #{created_by}"
+    "[#{created}] #{created_by || comment}"
   end
 end
--- spec/features/show_tag_spec.rb
+++ spec/features/show_tag_spec.rb
@@ -1,24 +1,34 @@
 require "rails_helper"
 
 feature "Tag details" do
-  scenario "Show details of tag", :vcr do
-    visit "/repo/test/hello-world"
+  shared_examples_for "successful showing an image tag" do
+    scenario "Show details of tag", :vcr do
+      visit "/repo/test/hello-world"
 
-    click_link "latest"
+      click_link "latest"
 
-    expect(page).to have_content "Tag"
-    expect(page).to have_content "hello-world:latest"
+      expect(page).to have_content "Tag"
+      expect(page).to have_content "hello-world:latest"
 
-    expect(page).to have_content "Content Digest"
-    expect(page.find_field("content_digest").value).to match /sha256:[0-9a-f]{64}/
+      expect(page).to have_content "Content Digest"
+      expect(page.find_field("content_digest").value).to match /sha256:[0-9a-f]{64}/
 
-    expect(page).to have_content "ENV"
-    expect(page).to have_content "IMAGE=test/hello-world:latest"
+      expect(page).to have_content "ENV"
+      expect(page).to have_content "IMAGE=test/hello-world:latest"
 
-    expect(page).to have_content "Labels"
-    expect(page).to have_content "image\ntest/hello-world:latest"
-    expect(page).to have_content "maintainer\nSomebody"
+      expect(page).to have_content "Labels"
+      expect(page).to have_content "image\ntest/hello-world:latest"
+      expect(page).to have_content "maintainer\nSomebody"
 
-    expect(page).to have_content /Layers\n001 sha256:[0-9a-f]{64}/
+      expect(page).to have_content /Layers\n001 sha256:[0-9a-f]{64}/
+    end
+  end
+
+  context "when the manifest is in oci v1 format" do
+    include_examples "successful showing an image tag"
+  end
+
+  context "when the manifest is in docker v2 format" do
+    include_examples "successful showing an image tag"
   end
 end
