721833220a4046273dc9f5b8ef7bd12992901466
Klaus Meyer <spam@klaus-meyer.net>
2023-10-04 12:07:09 +0200

    keep old puma concurrency config

--- config/puma.rb
+++ config/puma.rb
@@ -12,11 +12,11 @@ min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
 # Specifies that the worker count should equal the number of processors in production.
-if ENV["RAILS_ENV"] == "production"
-  require "concurrent-ruby"
-  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
-  workers worker_count if worker_count > 1
-end
+# if ENV["RAILS_ENV"] == "production"
+#   require "concurrent-ruby"
+#   worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
+#   workers worker_count if worker_count > 1
+# end
 
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
 # terminating a worker in development environments.
