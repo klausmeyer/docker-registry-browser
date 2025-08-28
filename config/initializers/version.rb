a47e514fb9f32e60c2d2ab1dfef34f2886aadd2a
Klaus Meyer <spam@klaus-meyer.net>
2025-08-28 18:19:29 +0200

    release 1.8.5
    * Restore "Next page" link in repo/image list (see #783)

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,4 +1,4 @@
-Rails.application.config.x.version = "1.8.4"
+Rails.application.config.x.version = "1.8.5"
 
 # Docker Cloud will pass the git commit it is building the image from
 # See: https://docs.docker.com/docker-hub/builds/advanced/
