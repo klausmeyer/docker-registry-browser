24ef4e8f8f240ec65c928febd860dcc2a66dce72
Klaus Meyer <spam@klaus-meyer.net>
2026-05-27 21:54:44 +0200

    Use rails default dockerfile and health-check

--- Gemfile
+++ Gemfile
@@ -33,7 +33,7 @@ gem "bootsnap", require: false
 # gem "kamal", require: false
 
 # Add HTTP asset caching/compression and X-Sendfile acceleration to Puma [https://github.com/basecamp/thruster/]
-# gem "thruster", require: false
+gem "thruster", require: false
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: "repositories#index"
 
-  get :ping, to: ->(env) { [ "200", { "Content-Type" => "text/plain" }, [ "pong" ] ] }
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
 
   get "repo/*repo/tag/*tag",    to: "tags#show",         as: :tag,         constraints: { repo: /.+/, tag: /[^\/]+/ }
   get "repo/*repo",             to: "repositories#show", as: :repository,  constraints: { repo: /.+/ }
